d71070d77ecfd0a7d3796b8eae75ef835422f3c6
f3tch <f3tch.169@gmail.com>
2026-06-13 15:15:30 +0500

    index init

---
+++ index.ts
@@ -0,0 +1,13 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "SmartVideoUploads",
+    description: "Automatically uploads oversized videos to a hosting service and sends a link instead.",
+    authors: [{ name: "f3tch", id: 1016388460929626174n }]
+});
