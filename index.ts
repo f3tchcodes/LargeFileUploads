2e0a1ebb77db5716980587f63c4570329b3e6937
f3tch <f3tch.169@gmail.com>
2026-06-13 17:47:28 +0500

    file upload patch + logging on a file upload event

--- index.ts
+++ index.ts
@@ -5,9 +5,27 @@
  */
 
 import definePlugin from "@utils/types";
+import { CloudUpload } from "@vencord/discord-types";
 
 export default definePlugin({
     name: "SmartVideoUploads",
     description: "Automatically uploads oversized videos to a hosting service and sends a link instead.",
-    authors: [{ name: "f3tch", id: 1016388460929626174n }]
+    authors: [{ name: "f3tch", id: 1016388460929626174n }],
+    patches: [
+        {
+            find: "async uploadFiles(",
+            replacement: [
+                {
+                    match: /async uploadFiles\((\i)\){/,
+                    replace: "$&$1.forEach($self.inspect);"
+                }
+            ],
+        }
+    ],
+    inspect(upload: CloudUpload) {
+        console.log("oh wow it's working");
+        console.log(upload);
+    }
 });
+
+
