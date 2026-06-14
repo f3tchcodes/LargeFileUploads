22eb7c1236b59e023c63ca424906f83a793bdc6e
f3tch <f3tch.169@gmail.com>
2026-06-14 14:15:47 +0500

    renaming index.tsx to index.ts

---
+++ index.ts
@@ -0,0 +1,36 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+import { CloudUpload as TCloudUpload } from "@vencord/discord-types";
+import { findLazy } from "@webpack";
+
+import { openConfirmModal } from "./utils/modals";
+export const CloudUpload: typeof TCloudUpload = findLazy(m => m.prototype?.trackUploadFinished);
+
+function stopUpload(upload: TCloudUpload) {
+    const { file } = upload.item;
+    upload.cancel();
+    openConfirmModal(file);
+}
+
+export default definePlugin({
+    name: "LargeFileUploads",
+    description: "Automatically uploads oversized files to a hosting service and sends a link instead.",
+    authors: [{ name: "f3tch", id: 1016388460929626174n }],
+    patches: [
+        {
+            find: "async uploadFiles(",
+            replacement: [
+                {
+                    match: /async uploadFiles\((\i)\){/,
+                    replace: "$&$1.forEach($self.stopUpload);"
+                }
+            ],
+        }
+    ],
+    stopUpload: stopUpload
+});
