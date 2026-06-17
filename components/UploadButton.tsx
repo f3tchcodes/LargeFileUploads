875a181bcb48d0b78074e4ebfabf488ea11b8d5a
f3tch <f3tch.169@gmail.com>
2026-06-17 14:43:31 +0500

    removing text and attachements when pressing the external upload button

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -5,14 +5,28 @@
  */
 
 import { ChatBarButton, ChatBarButtonFactory } from "@api/ChatButtons";
+import { findByPropsLazy } from "@webpack";
 import {
     DraftStore,
     DraftType,
+    openModal,
     React, UploadAttachmentStore,
     useStateFromStores
 } from "@webpack/common";
 
+import { SelectionModal } from "./SelectionModal";
 import { UploadIcon } from "./UploadIcon";
+
+export interface DraftActions {
+    clearDraft(channelId: string, DraftType): void;
+}
+
+export interface UploadActions {
+    clearAll(channelId: string, Drafttype): void;
+}
+
+const DraftActions = findByPropsLazy("clearDraft") as DraftActions;
+const UploadActions = findByPropsLazy("setUploads");
 
 const getDraft = (channelId: string) => DraftStore.getDraft(channelId, DraftType.ChannelMessage);
 let draftMessage = "";
@@ -36,7 +50,18 @@ export const UploadButton: ChatBarButtonFactory = ({ isAnyChat, isEmpty, type: {
     return (
         <ChatBarButton
             tooltip="Send via external upload!"
-            onClick={() => { }}
+            onClick={() => {
+                openModal(props => (
+                    <SelectionModal
+                        file={uploads[0]?.item.file}
+                        message={draftMessage}
+                        {...props}
+                    />
+                ));
+
+                DraftActions.clearDraft(channelId, DraftType.ChannelMessage);
+                UploadActions.clearAll(channelId, DraftType.ChannelMessage);
+            }}
         >
             <UploadIcon />
         </ChatBarButton>
